d8d4338c7cdc03c901aeb49394105d245e03b13a
Wildlife <admin@lanseyujie.com>
2022-04-07 18:11:55 +0800

    Add Feature 从环境变量中获取配置

--- dd/session.go
+++ dd/session.go
@@ -9,22 +9,42 @@ import (
 )
 
 type DingdongSession struct {
-	Address      Address      `json:"address"`
-	BarkId       string       `json:"bark_id"`
-	Client       *http.Client `json:"client"`
-	Cookie       string       `json:"cookie"`
-	Cart         Cart         `json:"cart"`
-	Order        Order        `json:"order"`
-	PackageOrder PackageOrder `json:"package_order"`
-	PayType      int          `json:"pay_type"`
-	CartMode     int          `json:"cart_mode"`
+	Address         Address      `json:"address"`
+	BarkId          string       `json:"bark_id"`
+	WeChatAppId     string       `json:"wechat_app_id"`
+	WeChatAppSecret string       `json:"wechat_app_secret"`
+	Client          *http.Client `json:"client"`
+	Cookie          string       `json:"cookie"`
+	Cart            Cart         `json:"cart"`
+	Order           Order        `json:"order"`
+	PackageOrder    PackageOrder `json:"package_order"`
+	PayType         int          `json:"pay_type"`
+	CartMode        int          `json:"cart_mode"`
+	NoticeMode      int          `json:"notice_mode"`
 }
 
-func (s *DingdongSession) InitSession(cookie string, barkId string) error {
+func (s *DingdongSession) InitSession() error {
 	fmt.Println("########## 初始化 ##########")
 	s.Client = &http.Client{}
-	s.Cookie = cookie
-	s.BarkId = barkId
+	// https://activity.m.ddxq.mobi/#/coupon?code=VERxg&h5_source=caocao&btnType=jumpApp&path=https://u.100.me/m/maicai&random=4
+	// 电脑浏览器从此地址登录后即可从 Cookie 取得 DDXQSESSID
+	s.Cookie = os.Getenv("DDXQSESSID")
+	if s.Cookie == "" {
+		return errors.New("没有配置 DDXQSESSID 环境变量")
+	}
+	s.Cookie = "DDXQSESSID=" + s.Cookie
+
+	s.BarkId = os.Getenv("BARKID")
+	s.WeChatAppId = os.Getenv("WECHATAPPID")
+	s.WeChatAppSecret = os.Getenv("WECHATAPPSECRET")
+
+	// 根据不同环境变量设置通知方式，不设置不通知
+	if s.BarkId != "" {
+		s.NoticeMode = 1
+	} else if s.WeChatAppId != "" && s.WeChatAppSecret != "" {
+		s.NoticeMode = 2
+	}
+
 	err, addrList := s.GetAddress()
 	if err != nil {
 		return err
